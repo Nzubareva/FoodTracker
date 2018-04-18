b48154f2b60f3651cc9de3b4f517f70cee21b700
Natallia Zubareva <Ndobrovolskaya@gmail.com>
2018-04-18 17:02:40 +0300

    Design Custom table cells
    Design the interface of the custom table cell, add an Image View, Label and Horizontal Stack view for rating control

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Natallia Zubareva on 18.04.2018.
+//  Copyright © 2018 Natallia. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
